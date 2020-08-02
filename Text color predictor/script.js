ddb4cc28ab8758670fa22417eb1fd5e4fdb3b472
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-08-02 19:43:41 +0530

    Particle.JS built

---
+++ Text color predictor/script.js
@@ -0,0 +1,15 @@
+const net  = new brain.NeuralNetwork();
+
+const data = [
+{
+	input : {r: 0, g: 0, b: 0},
+	output: [1]
+},
+{
+	input : {r: 1, g: 1, b: 1},
+	output: [0]
+}]
+
+net.train(data);
+
+console.log(net.run({r: 1, g: .5, b: 1}))
